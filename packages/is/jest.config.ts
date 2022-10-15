5d0c9ffe363028c8a0d11d128295ac6a60d1a99c
Jonathan Faircloth <jonathan@faircloth.xyz>
2022-10-15 11:31:59 -0400

    chore(repo): bump nrwl/nx version

--- packages/is/jest.config.ts
+++ packages/is/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'collections',
+/* eslint-disable */
+export default {
+  displayName: 'is',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +12,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/collections',
+  coverageDirectory: '../../coverage/packages/is',
 };
