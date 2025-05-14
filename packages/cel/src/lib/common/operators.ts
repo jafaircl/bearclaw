d43464c6aa055afd477a63d57c3b9f17c97cd020
jafaircl <jafaircl@me.com>
2025-05-13 21:18:47 -0400

    feat(cel): add unparser

--- packages/cel/src/lib/common/operators.ts
+++ packages/cel/src/lib/common/operators.ts
@@ -89,3 +89,64 @@ export function getOperatorFromText(text: string): string | undefined {
       return undefined;
   }
 }
+
+const operatorMap = new Map<
+  string,
+  { displayName: string; precedence: number; arity: number }
+>([
+  [CONDITIONAL_OPERATOR, { displayName: '', precedence: 8, arity: 3 }],
+  [LOGICAL_OR_OPERATOR, { displayName: '||', precedence: 7, arity: 2 }],
+  [LOGICAL_AND_OPERATOR, { displayName: '&&', precedence: 6, arity: 2 }],
+  [EQUALS_OPERATOR, { displayName: '==', precedence: 5, arity: 2 }],
+  [GREATER_OPERATOR, { displayName: '>', precedence: 5, arity: 2 }],
+  [GREATER_EQUALS_OPERATOR, { displayName: '>=', precedence: 5, arity: 2 }],
+  [LESS_OPERATOR, { displayName: '<', precedence: 5, arity: 2 }],
+  [LESS_EQUALS_OPERATOR, { displayName: '<=', precedence: 5, arity: 2 }],
+  [NOT_EQUALS_OPERATOR, { displayName: '!=', precedence: 5, arity: 2 }],
+  [IN_OPERATOR, { displayName: 'in', precedence: 5, arity: 2 }],
+  [ADD_OPERATOR, { displayName: '+', precedence: 4, arity: 2 }],
+  [SUBTRACT_OPERATOR, { displayName: '-', precedence: 4, arity: 2 }],
+  [DIVIDE_OPERATOR, { displayName: '/', precedence: 3, arity: 2 }],
+  [MODULO_OPERATOR, { displayName: '%', precedence: 3, arity: 2 }],
+  [MULTIPLY_OPERATOR, { displayName: '*', precedence: 3, arity: 2 }],
+  [LOGICAL_NOT_OPERATOR, { displayName: '!', precedence: 2, arity: 1 }],
+  [NEGATE_OPERATOR, { displayName: '-', precedence: 2, arity: 1 }],
+  [INDEX_OPERATOR, { displayName: '', precedence: 1, arity: 2 }],
+  [OPT_INDEX_OPERATOR, { displayName: '', precedence: 1, arity: 2 }],
+  [OPT_SELECT_OPERATOR, { displayName: '', precedence: 1, arity: 2 }],
+]);
+
+export function findReverse(operator: string) {
+  const op = operatorMap.get(operator);
+  if (!op) {
+    return '';
+  }
+  return op.displayName;
+}
+
+export function findReverseBinaryOperator(symbol: string) {
+  const op = operatorMap.get(symbol);
+  if (!op || op.arity !== 2) {
+    return '';
+  }
+  if (op.displayName === '') {
+    return '';
+  }
+  return op.displayName;
+}
+
+export function precedence(symbol: string) {
+  const op = operatorMap.get(symbol);
+  if (!op) {
+    return 0;
+  }
+  return op.precedence;
+}
+
+export function arity(symbol: string) {
+  const op = operatorMap.get(symbol);
+  if (!op) {
+    return -1;
+  }
+  return op.arity;
+}
